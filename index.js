9bff6d536b54819888829722024cc711196061b5
Miguel Fuentes <miguel.fuentes.sobrin@gmail.com>
2021-12-10 17:46:20 +0100

    minor fixes

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
 document.body.onload = init();
 
-/* Load external Google Font */
-function loadExternalFonts() {
-  const link = document.createElement('link');
-  link.setAttribute('rel', 'stylesheet');
-  link.setAttribute('type', 'text/css');
-  link.setAttribute('href', 'https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;500&display=swap');
-  document.head.appendChild(link);
-}
-
+/* Load widget styles from external file */
 function loadWidgetStyles() {
   const link = document.createElement('link');
   link.setAttribute('rel', 'stylesheet');
   link.setAttribute('type', 'text/css');
-  link.setAttribute('href', 'https://cdn.jsdelivr.net/gh/miguelFs86/jsTest/main/index.min.css');
+  // link.setAttribute('href', 'https://cdn.jsdelivr.net/gh/miguelFs86/jsTest/main/index.css');
+  link.setAttribute('href', 'https://ghcdn.rawgit.org/MiguelFs86/jsTest/main/index.css');
   document.head.appendChild(link);
+  link.onload = () => {
+    createWidget();
+  };
 }
 
 /* Set widget title */
@@ -27,53 +23,19 @@ function getDivText() {
   return 'Lorem ipsum dolor sit amet consectetur adipiscing elit, facilisi ridiculus porttitor mi cursus condimentum ut mauris, diam vulputate quisque euismod aptent dui. Curabitur himenaeos vitae quis tellus dictum fringilla fusce semper, pulvinar per eu facilisis nisl habitasse at tincidunt commodo, magna auctor ultrices suspendisse nostra eros orci. Ad massa enim natoque est vitae senectus gravida at viverra ornare, congue ut urna pretium porta diam sagittis rutrum libero, sociosqu pellentesque id luctus lacinia accumsan litora fermentum varius.';
 }
 
-/* Apply styles for widget main div */
-function applyDivStyles(element) {
-  element.id = 'widget-main-div';
-  element.style.zIndex = 99999;
-  element.style.position = 'absolute';
-  element.style.top = '100px';
-  element.style.left = '200px';
-  element.style.width = '350px';
-  element.style.padding = '20px';
-  element.style.boxShadow = 'rgba(100, 100, 111, 0.2) 0px 7px 29px 0px';
-  element.style.fontFamily = 'Cairo, sans-serif';
-  element.style.backgroundColor = "white";
-}
-
-/* Apply styles for widget title */
-function applyTitleStyles(element) {
-  element.id = 'widget-div-title';
-  element.style.fontWeight = 'bold';
-  element.style.textDecoration = 'underline';
-  element.style.margin = '0';
-  element.style.fontSize = '1.8rem';
-  element.style.color = '#34558b'
-}
-
-/* Apply styles for widget content */
-function applyContentStyles(element) {
-  element.id = 'widget-div-content';
-  element.style.textAlign = 'justify';
-  element.style.lineHeight = '1.6rem';
-  element.style.color = '#798fa8';
-}
-
 /* Create the widget elements */
-function init() {
-  loadExternalFonts();
+function createWidget() {
   const divContentTitle = getDivTitle();
   const divContentText = getDivText();
   
   /* Create the HTML elements */
-  const divContainer = document.createElement("div");
-  const divTitle = document.createElement("p");
-  const divContent = document.createElement("p");
+  const divContainer = document.createElement('div');
+  const divTitle = document.createElement('p');
+  const divContent = document.createElement('p');
 
-  /* Apply the styles */
-  applyDivStyles(divContainer);
-  applyTitleStyles(divTitle);
-  applyContentStyles(divContent);
+  divContainer.id = 'widget-main-div';
+  divTitle.id = 'widget-div-title';
+  divContent.id = 'widget-div-content';
   
   /* Append the title and content to the main DIV */
   divContainer.appendChild(divTitle);
@@ -90,3 +52,6 @@ function init() {
   document.body.appendChild(divContainer);
 }
 
+function init() {
+  loadWidgetStyles();
+}
