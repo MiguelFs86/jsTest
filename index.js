683ce070f3105f6d4dea0050fd059028c3dc58ab
Miguel Fuentes <miguel.fuentes.sobrin@gmail.com>
2021-12-10 17:55:24 +0100

    fix cdn links

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ function loadWidgetStyles() {
   const link = document.createElement('link');
   link.setAttribute('rel', 'stylesheet');
   link.setAttribute('type', 'text/css');
-  // link.setAttribute('href', 'https://cdn.jsdelivr.net/gh/miguelFs86/jsTest/main/index.css');
-  link.setAttribute('href', 'https://ghcdn.rawgit.org/MiguelFs86/jsTest/main/index.css');
+  link.setAttribute('href', 'https://cdn.jsdelivr.net/gh/miguelFs86/jsTest/main/index.css');
   document.head.appendChild(link);
   link.onload = () => {
     createWidget();
