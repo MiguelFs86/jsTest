b958351dbec64cff2b48f5a66b7bd6aed1d19abd
Miguel Fuentes <miguel.fuentes.sobrin@gmail.com>
2022-01-10 15:41:28 +0100

    add global service to store widgets config

---
+++ sep_widget_config.js
@@ -0,0 +1,19 @@
+const SEPWidgetConfig = {
+  colors: {
+    mainGreen: '#00aa3e',
+    mainBlue: '#94c3e3',
+    mainGray: '#a0a1a3'
+  },
+  lineChartConfig : {
+    lineThickness: 1,
+    pointRadius: 1.5
+  },
+  fontSize: '1rem'
+}
+
+const consumptionFactor = {
+  houseHoldFactor: 1/15.3,
+  hairDryer: 1/2,
+  fridge: 1/0.15,
+  computer: 1/0.085,
+}
